ebed98446a6e0f6a30b66a5fca9f4543fbcc8aec
杨金 <yangjin.im@bytedance.com>
2021-08-23 03:27:48 +0800

    refactor: use MutationObserver to replace setInterval

--- js/injected-script.js
+++ js/injected-script.js
@@ -1,10 +1,39 @@
-let __logMessageTimerId;
+let __catcherIframeObserver = null;
+const __catcherObserverCallback = __catcherThrottle(__logPostMessage, 300);
+
+function __catcherThrottle(fn, interval) {
+  let last = 0;
+  let timer = null;
+  return function () {
+    let now = Date.now();
+    let delay = interval - (now - last);
+    if (delay <= 0) {
+      if (timer) {
+        window.clearTimeout(timer);
+        timer = null;
+      }
+      last = Date.now();
+      fn.apply(this, arguments);
+    } else {
+      if (timer) window.clearTimeout(timer);
+      timer = setTimeout(() => {
+        last = Date.now();
+        fn.apply(this, arguments);
+      }, delay)
+    }
+  }
+}
 
 /**
  * log posted message
  */
-function __logPostMessage() {
+function __logPostMessage(mutationsList, observer) {
   const iframeList = Array.from(document.querySelectorAll('iframe'));
+  
+  if (!iframeList.length) {
+    return;
+  }
+
   iframeList.forEach(iframe => {
     try {
       const { postMessage, __catchPostMessage } = iframe.contentWindow;
@@ -27,9 +56,9 @@ function __logPostMessage() {
 }
 
 function __closeLogPostMessage() {
-  if (__logMessageTimerId) {
-    clearInterval(__logMessageTimerId);
-    __logMessageTimerId = null;
+  if (__catcherIframeObserver) {
+    __catcherIframeObserver.disconnect();
+    __catcherIframeObserver = null;
   }
   const iframeList = Array.from(document.querySelectorAll('iframe'));
   iframeList.forEach(iframe => {
@@ -47,10 +76,20 @@ function __closeLogPostMessage() {
 }
 
 function __openLogPostMessage() {
-  __logPostMessage();
-  __logMessageTimerId = setInterval(__logPostMessage, 500);
+  __catcherIframeObserver = __catcherCreateIframeObserver(__catcherObserverCallback);
 }
 
+function __catcherCreateIframeObserver(callback) {
+  const targetNode = document.body;
+  const config = {
+    attributes: false,
+    childList: true,
+    subtree: true
+  };
+  const observer = new MutationObserver(callback);
+  observer.observe(targetNode, config);
+  return observer;
+}
 
 window.addEventListener("message", function (e) {
   const { type, data } = e.data || {};
